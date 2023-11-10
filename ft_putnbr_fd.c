6bc63d6f282d8770a9dcabdc0dbde9e16faf513f
Decity <eyup.alibayat@gmail.com>
2023-11-10 13:36:30 +0100

    fixes

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ebayat <ebayat@student.42.fr>              +#+  +:+       +#+        */
+/*   By: elie <elie@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 15:31:29 by ebayat            #+#    #+#             */
-/*   Updated: 2023/11/09 16:24:20 by ebayat           ###   ########.fr       */
+/*   Updated: 2023/11/10 13:35:15 by elie             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,17 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
+	if (n == -2147483648)
+	{
+		ft_putstr_fd("-2147483648", fd);
+		return ;
+	}
 	if (n < 0)
 	{
 		ft_putchar_fd('-', fd);
 		n *= -1;
 	}
 	if (n >= 10)
-	{
 		ft_putnbr_fd(n / 10, fd);
-	}
 	ft_putchar_fd(('0' + n % 10), fd);
 }
--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: elie <elie@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 14:40:54 by ebayat            #+#    #+#             */
-/*   Updated: 2023/11/09 13:43:23 by elie             ###   ########.fr       */
+/*   Updated: 2023/11/10 13:27:44 by elie             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,13 @@
 
 char	*ft_strdup(const char *s)
 {
+	size_t	len;
 	char	*new_str;
 
-	new_str = (char *) malloc((ft_strlen(s) + 1) * sizeof(char));
+	len = ft_strlen(s) + 1;
+	new_str = (char *)malloc(len * sizeof(char));
 	if (!new_str)
 		return (NULL);
-	ft_strlcpy(new_str, s, ft_strlen(s));
+	ft_strlcpy(new_str, s, len);
 	return (new_str);
 }
